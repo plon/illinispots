f586492283a48563cbc876d4fd56ec9493fd52bb
Evan Chiao <chiaoevan@gmail.com>
2026-08-15 03:10:49 +0000

    Redirect www traffic to illinispots.com

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,21 @@
 import { withSentryConfig } from "@sentry/nextjs";
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: "/:path*",
+        has: [
+          {
+            type: "host",
+            value: "www.illinispots.com",
+          },
+        ],
+        destination: "https://illinispots.com/:path*",
+        permanent: true,
+      },
+    ];
+  },
   images: {
     remotePatterns: [
       {
